24f4de6b9affd3734bfe0561d36f566117eabb89
kang junemo <103883786+peter0107@users.noreply.github.com>
2022-12-01 02:31:23 +0900

    send.html에 추가

--- http.js
+++ http.js
@@ -3,38 +3,47 @@ const parser=require("node-html-parser");
 
 
 
-let url="https://www.musinsa.com/search/musinsa/integration?type=&q="+"패딩";
 
-https.get(url,(res)=>{
-    let data="";
-    res.on("data",(d)=>{
-        data+=d;
+
+function Get_Info(){
+    let url="https://www.musinsa.com/search/musinsa/integration?type=&q="+"패딩";
+    https.get(url,(res)=>{
+        let data="";
+        res.on("data",(d)=>{
+            data+=d;
+        })
+        res.on("end",()=>{
+            
+            //브랜드
+            let root=parser.parse(data);
+            root.querySelectorAll(".item_title").forEach((list)=>{
+                console.log(list.innerText.trim());
+                document.getElementById('brand').value=list[0].innerText.trim();
+            });
+            console.log("");
+            //제품이름
+            root.querySelectorAll(".list_info a[title]").forEach((list)=>{
+                console.log(list.innerText.trim());
+                document.getElementById("name").value=list[0].innerText.trim();
+            });
+            console.log("");
+            //가격
+            root.querySelectorAll(".price").forEach((list)=>{
+                console.log(list.innerText.trim());
+                document.getElementById("price").value=list[0].innerText.trim();
+            });
+            console.log("");
+            //사진 url
+            root.querySelectorAll("div.list_img > a > img.lazyload.lazy").forEach((list)=>{
+                console.log(list.attributes['data-original']);
+            });
+    
+        });
     })
-    res.on("end",()=>{
-        
-        //브랜드
-        let root=parser.parse(data);
-        root.querySelectorAll(".item_title").forEach((list)=>{
-            console.log(list.innerText.trim());
-        });
-        console.log("");
-        //제품이름
-        root.querySelectorAll(".list_info a[title]").forEach((list)=>{
-            console.log(list.innerText.trim());
-        });
-        console.log("");
-        //가격
-        root.querySelectorAll(".price").forEach((list)=>{
-            console.log(list.innerText.trim());
-        });
-        console.log("");
-        //사진 url
-        root.querySelectorAll("div.list_img > a > img.lazyload.lazy").forEach((list)=>{
-            console.log(list.attributes['data-original']);
-        });
+}
 
-    });
-})
+
+document.getElementById('test').onclick=Get_Info;
 
 
 
