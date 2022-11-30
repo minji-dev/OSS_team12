68ec05615fefc1b862b1df8d19944f33ebceaec5
mj.park <gbsmj0312@gmail.com>
2022-11-30 21:58:47 +0900

    [Fix] public connect

--- app.js
+++ app.js
@@ -6,12 +6,10 @@ const app = express();
 // static 폴더 설정
 app.use(express.static(`${__dirname}/public`));
 
-// 화면 engine을 ejs로 설정
-app.set('view engine', 'ejs');
+// 화면 engine을 html로 설정
+app.set("views", "./views");
+// app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
-
-// view 경로 설정
-app.set('views', __dirname + '/views');
 
 // 라우팅
 const home = require("./routes");
