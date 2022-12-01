14d6b0ff3c270b60b2841a94405c74051016a751
kang junemo <103883786+peter0107@users.noreply.github.com>
2022-12-02 01:42:25 +0900

    bug in air

--- routes/index.js
+++ routes/index.js
@@ -8,19 +8,22 @@ const https=require("https");
 const parser=require("node-html-parser");
 const { send } = require('process');
 
-let weather;
+let lat;
+let lon;
+let weather_info;
+let air_info;
 let clothe_info = [];
 
-const api = "https://api.openweathermap.org/data/2.5/weather";
+const api = "https://api.openweathermap.org/data/2.5/";
 const apiKey = conf.SERVICE_KEY;
 
 router.get('/', (req, res) => {
     res.render('index', {
         weather: null,
-        descript: null,
         loc: 'Locating...',
         temp: null,
         icon: null,
+        air: null,
         error: null,
     });
 });
@@ -62,51 +65,52 @@ router.get('/musinsa',(req,res)=>{
     
 })
 
-router.get('/weather', (req, res) => {
-    let lat = req.query.lat;
-    let lon = req.query.lon;
-    let url = api+`?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
+
+router.get('/weather', (req, res, next) => {
+    lat = req.query.lat;
+    lon = req.query.lon;
+    let url = api+`weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
 
     request(url, async (err, res2, body) => {
         if (err) {
             res.render('index', { weather: null, temp: null, loc: 'Error, please try again' });
         } else {
-            weather = await JSON.parse(body);
-            if (weather.main == undefined) {
+            weather_info = await JSON.parse(body);
+            if (weather_info.main == undefined) {
                 res.render('index', { weather: null, temp: null, loc: 'Error, please try again' });
             } else {
                 //기온에 따른 옷차림
-                if(weather.main.temp<4){
+                if(weather_info.main.temp<4){
                     clothe_info.push('패딩');
                     clothe_info.push('코트');
                     clothe_info.push('목도리');
                 }
-                else if(weather.main.temp<8){
+                else if(weather_info.main.temp<8){
                     clothe_info.push('코트');
                     clothe_info.push('가죽자켓');
                     clothe_info.push('니트');
                 }
-                else if(weather.main.temp<11){
+                else if(weather_info.main.temp<11){
                     clothe_info.push('트렌치코트');
                     clothe_info.push('야상');
                     clothe_info.push('니트');
                 }
-                else if(weather.main.temp<16){
+                else if(weather_info.main.temp<16){
                     clothe_info.push('가디건');
                     clothe_info.push('야상');
                     clothe_info.push('면바지');
                 }
-                else if(weather.main.temp<19){
+                else if(weather_info.main.temp<19){
                     clothe_info.push('얇은 니트');
                     clothe_info.push('맨투맨');
                     clothe_info.push('청바지');
                 }
-                else if(weather.main.temp<22){
+                else if(weather_info.main.temp<22){
                     clothe_info.push('얇은 가디건');
                     clothe_info.push('긴팔');
                     clothe_info.push('면바지');
                 }
-                else if(weather.main.temp<27){
+                else if(weather_info.main.temp<27){
                     clothe_info.push('반팔');
                     clothe_info.push('얇은 셔츠');
                     clothe_info.push('반바지');
@@ -118,16 +122,29 @@ router.get('/weather', (req, res) => {
                 }
             }
         }
+        next();
+    })
+}, (req, res, next) => {
+    let url = api+`air_pollution?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
+    request(url, async (err, res3, body) => {
+        if (err) {
+            res.render('index', { weather: null, temp: null, loc: 'Error, please try again' });
+        } else {
+            air_info = await JSON.parse(body);
+            if (air_info.list == undefined) {
+                res.render('index', { weather: null, temp: null, loc: 'Error, please try again' });
+            }
+        }
     })
 });
 
 router.get('/main', (req, res) => {
     res.render('snd', {
-        weather: weather.weather[0].main,
-        descript: weather.weather[0].description,
-        loc: weather.name,
-        temp: weather.main.temp,
-        icon: weather.weather.icon,
+        weather: weather_info.weather[0].main,
+        loc: weather_info.name,
+        temp: weather_info.main.temp,
+        icon: weather_info.weather.icon,
+        air: air_info.list[0].main.aqi,
         clothe_info: clothe_info,
         error: null,
     });
