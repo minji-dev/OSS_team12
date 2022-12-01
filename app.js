4c21a6f43873d6a0a48ac2b302625bbab181c663
Minji Park <gbsmj0312@gmail.com>
2022-12-01 14:19:51 +0900

    weather & front success (#11)
    * close issue #4
    
    * from local, branch generated
    
    * second page generated
    
    * Made weather data as table
    
    * Modified table's position
    
    * [Fix] public connect
    
    * 날씨에 따른 아이콘 변경
    
    * 일단 푸쉬
    
    * problem solved
    
    * 크롤링 위한 id 생성 완료 (document.getElementById(<id>))
    
    * [Edit] url request code -> back
    
    * [Fix] description hidden error
    
    * [Edit] / and /main division
    
    * [Delete] Json file
    
    * index.html contents moved to index.ejs
    
    * nodemon --> node
    
    * merge crawling branch (#9)
    
    * crawling test commit
    
    * 사진url빼고 다가져옴
    
    * 중간에 저장
    
    * 이미지까지 완료
    
    * send.html에 추가
    
    * 무신사크롤링 html에 올리는중
    
    Co-authored-by: kang junemo <103883786+peter0107@users.noreply.github.com>
    
    * [Fix] front-weather connect
    
    * [Fix] connect root-main page
    
    * merge back to front
    
    Co-authored-by: LsaacIee <leejihak02@gmail.com>
    Co-authored-by: kang junemo <103883786+peter0107@users.noreply.github.com>

--- app.js
+++ app.js
@@ -6,12 +6,9 @@ const app = express();
 // static 폴더 설정
 app.use(express.static(`${__dirname}/public`));
 
-// 화면 engine을 ejs로 설정
+// 화면 엔진 ejs 설정
+app.set("views", "./views");
 app.set('view engine', 'ejs');
-app.engine('html', require('ejs').renderFile);
-
-// view 경로 설정
-app.set('views', __dirname + '/views');
 
 // 라우팅
 const home = require("./routes");
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,54 @@
 const express = require('express');
 const router = express.Router();
 
+const request = require('request');
+const conf = require('../conf/conf');
+
+let weather;
+
+const api = "https://api.openweathermap.org/data/2.5/weather";
+const apiKey = conf.SERVICE_KEY;
+
 router.get('/', (req, res) => {
-    return res.render('index.html')
+    res.render('index', {
+        weather: null,
+        descript: null,
+        loc: 'Locating...',
+        temp: null,
+        icon: null,
+        error: null,
+    });
+});
+
+router.get('/weather', (req, res) => {
+    let lat = req.query.lat;
+    let lon = req.query.lon;
+    let url = api+`?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
+
+    request(url, async (err, res2, body) => {
+        if (err) {
+            res.render('index', { weather: null, temp: null, loc: 'Error, please try again' });
+        } else {
+            weather = await JSON.parse(body);
+            if (weather.main == undefined) {
+                res.render('index', { weather: null, temp: null, loc: 'Error, please try again' });
+            } else {
+                console.log(weather);
+                return res.redirect('http://localhost:3000/main');
+            }
+        }
+    })
+});
+
+router.get('/main', (req, res) => {
+    res.render('snd', {
+        weather: weather.weather[0].main,
+        descript: weather.weather[0].description,
+        loc: weather.name,
+        temp: weather.main.temp,
+        icon: weather.weather.icon,
+        error: null,
+    });
 });
 
 module.exports = router;
